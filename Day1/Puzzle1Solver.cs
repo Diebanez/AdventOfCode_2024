91a35caa307b14c74cb143233137fcfd02fb4806
Diego Cugno <diego.cugno@tinybullstudios.com>
2024-12-01 22:00:26 +0100

    Added Day 1 solution

---
+++ Day1/Puzzle1Solver.cs
@@ -0,0 +1,27 @@
+﻿namespace Day1;
+
+public static class Puzzle1Solver
+{
+    public static void Solve(string[] lines)
+    {
+        var leftValues = new List<int>();
+        var rightValues = new List<int>();
+
+        foreach (var line in lines)
+        {
+            leftValues.Add(int.Parse(line.Split("   ")[0]));
+            rightValues.Add(int.Parse(line.Split("   ")[1]));
+        }
+        
+        leftValues.Sort();
+        rightValues.Sort();
+
+        var total = 0;
+        for (int i = 0; i < leftValues.Count; i++)
+        {
+            total += Math.Abs(leftValues[i] - rightValues[i]);
+        }
+        
+        Console.WriteLine(total);
+    }
+}
---
+++ Day1/Puzzle2Solver.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1;
+
+public static class Puzzle2Solver
+{
+    public static void Solve(string[] lines)
+    {
+        var leftValues = new List<int>();
+        var rightCount = new Dictionary<int, int>();
+
+        foreach (var line in lines)
+        {
+            leftValues.Add(int.Parse(line.Split("   ")[0]));
+            var rightValue = int.Parse(line.Split("   ")[1]);
+            if (!rightCount.ContainsKey(rightValue))
+            {
+                rightCount.Add(rightValue, 0);
+            }
+            rightCount[rightValue]++;
+        }
+        
+
+        var total = 0;
+        foreach (var value in leftValues)
+        {
+            var multiplier = rightCount.GetValueOrDefault(value, 0);
+            total += value * multiplier;
+        }
+        
+        Console.WriteLine(total);
+    }
+}
