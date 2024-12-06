ae125e9c1aac1beb0020a5e9aaf0743db062f4cb
Diego Cugno <diego.cugno@tinybullstudios.com>
2024-12-06 13:46:53 +0100

    Added Day 3 solution

---
+++ Day3/Puzzle1Solver.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
+
+public class Puzzle1Solver
+{
+    public static void Solve(string[] lines)
+    {
+        var result = 0;
+        foreach (var line in lines)
+        {
+            // Find all matches
+            var matches = Regex.Matches(line, @"mul\(\d+,\d+\)");
+            foreach (Match match in matches)
+            {
+                var values = Regex.Matches(match.Value, @"\d+");
+                var mulResult = values.Select(x => int.Parse(x.Value)).Aggregate((total, value) => total * value);
+                result += mulResult;
+            }
+        }
+        
+        Console.WriteLine(result);
+    }
+}
---
+++ Day3/Puzzle2Solver.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Day3;
+
+public class Puzzle2Solver
+{
+    public static void Solve(string[] lines)
+    {
+        const string doText = "do()";
+        const string dontText = "don't()";
+
+        var enabled = true;
+        var textToEvaluate = new StringBuilder();
+        foreach (var line in lines)
+        {
+            for (var i = 0; i < line.Length; i++)
+            {
+                if (enabled)
+                {
+                    if (i + dontText.Length < line.Length && line.Substring(i, dontText.Length) == dontText)
+                    {
+                        enabled = false;
+                        i += dontText.Length;
+                        continue;
+                    }
+
+                    textToEvaluate.Append(line[i]);
+                }
+                else
+                {
+                    if (i + doText.Length >= line.Length || line.Substring(i, doText.Length) != doText) continue;
+                    enabled = true;
+                    i += doText.Length;
+                }
+            }
+        }
+
+        Puzzle1Solver.Solve([textToEvaluate.ToString()]);
+    }
+}
