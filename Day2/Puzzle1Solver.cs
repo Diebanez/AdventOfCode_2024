97534d03a1fe690d9bceb09416d0244149afef50
Diego Cugno <diego.cugno@tinybullstudios.com>
2024-12-06 13:15:26 +0100

    Added Day 2 solution

---
+++ Day2/Puzzle1Solver.cs
@@ -0,0 +1,36 @@
+﻿namespace Day2;
+
+public class Puzzle1Solver
+{
+    public static void Solve(string[] lines)
+    {
+        var counter = 0;
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(' ').Select(int.Parse).ToArray();
+            var valid = true;
+            var increasing = numbers[0] < numbers[1];
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                var delta = numbers[i] - numbers[i + 1];
+                if (Math.Abs(delta) is > 0 and <= 3)
+                {
+                    if ((increasing && delta < 0) || (!increasing && delta > 0))
+                    {
+                        continue;
+                    }
+                }
+
+                valid = false;
+                break;
+            }
+
+            if (valid)
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
---
+++ Day2/Puzzle2Solver.cs
@@ -0,0 +1,53 @@
+﻿namespace Day2;
+
+public class Puzzle2Solver
+{
+    public static void Solve(string[] lines)
+    {
+        var counter = 0;
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(' ').Select(int.Parse).ToArray();
+            if (IsValid(numbers))
+            {
+                counter++;
+                continue;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var alternative = numbers.ToList();
+                alternative.RemoveAt(i);
+                if (IsValid(alternative.ToArray()))
+                {
+                    counter++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+
+    private static bool IsValid(int[] numbers)
+    {
+        var valid = true;
+        var increasing = numbers[0] < numbers[1];
+        for (var i = 0; i < numbers.Length - 1; i++)
+        {
+            var delta = numbers[i] - numbers[i + 1];
+            if (Math.Abs(delta) is > 0 and <= 3)
+            {
+                if ((increasing && delta < 0) || (!increasing && delta > 0))
+                {
+                    continue;
+                }
+            }
+
+            valid = false;
+            break;
+        }
+
+        return valid;
+    }
+}
